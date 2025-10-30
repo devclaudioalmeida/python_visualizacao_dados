59435f82d4e6876ca9064dd8be837f2cbe04b285
devclaudioalmeida <claudio.almeida.webmarketing@gmail.com>
2025-10-30 12:52:35 -0300

    Continuação da prática

--- mpl_quadrados.py
+++ mpl_quadrados.py
@@ -1,12 +1,25 @@
 import matplotlib.pyplot as plt
 
-quadrados = []
+def plota_grafico(pontos):
+    """Função simples para plotar um gráfico de função quadrática na tela"""
 
-for i in range (1, 10):
-    n = (i) ** 2
-    quadrados.append(n)
+    # Gera a lista de pontos para o gráfico
+    quadrados = []
+    for i in range (1, pontos+1):
+        n = i ** 2
+        quadrados.append(n)
+
+    fig, ax = plt.subplots()
+    ax.plot(quadrados, linewidth=3)
+    # Define o título do gráfico e os rótulos dos eixos
+    str = f'Função com os {pontos} primeiros quadrados'
+    ax.set_title(str, fontsize=18)
+    ax.set_xlabel('Valores', fontsize=12)
+    ax.set_ylabel('Quadrados dos valores', fontsize=12)
+    # Define o tamano dos rótulos de marcação de escala
+    ax.tick_params(labelsize=14)
+    # Exibe o gráfico na tela
+    plt.show()
 
 
-fig, ax = plt.subplots()
-ax.plot(quadrados)
-plt.show()
+plota_grafico(100)
